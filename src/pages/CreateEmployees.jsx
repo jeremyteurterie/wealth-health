a86e900edac65c14e75763967e968a849b421706
Jeremy <jeremy.teurterie@gmail.com>
2023-05-04 09:40:48 +0700

    :sparkles: feat: wip employees page

--- src/pages/CreateEmployees.jsx
+++ src/pages/CreateEmployees.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // styles
 import styles from '../styles/CreateEmployees.module.css';
+// components
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+// styles
+import styles from '../styles/Employees.module.css';
+// components
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Employees = () => {
-  return <div></div>;
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <div id="employee-div" class={styles.container}>
+          <h1>Current Employees</h1>
+          <table id="employee-table" class={styles.display}></table>
+          {/* <a href="index.html">Home</a> */}
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 };
 
 export default Employees;
