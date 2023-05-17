48b4e15af82662bffe6a9b1dbd5bbb133761c57e
Jeremy <jeremy.teurterie@gmail.com>
2023-05-17 17:57:51 +0700

    :heavy_minus_sign: Chore: remove dependencies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 // styles
 import styles from '../styles/Home.module.css';
 // components
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import DateInput from '../components/DateInput';
 
 const Home = () => {
   return (
@@ -21,12 +20,10 @@ const Home = () => {
             <input type="text" id="last-name" />
 
             <label htmlFor="date-of-birth">Date of Birth</label>
-            <DateInput name="date-of-birth" />
 
             <label htmlFor="start-date">Start Date</label>
-            <DateInput name="start-date" />
 
-            <fieldset class="address">
+            <fieldset className="address">
               <legend>Address</legend>
 
               <label htmlFor="street">Street</label>
@@ -51,11 +48,9 @@ const Home = () => {
               <option>Legal</option>
             </select>
           </form>
-          <button className={styles.button} onclick="saveEmployee()">
-            Save
-          </button>
+          <button className={styles.button}>Save</button>
         </div>
-        <div id="confirmation" class={styles.modal}>
+        <div id="confirmation" className={styles.modal}>
           Employee Created!
         </div>
       </main>
