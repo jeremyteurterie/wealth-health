d5acfd50740848a04cdb0103fd11766655162426
Jeremy <jeremy.teurterie@gmail.com>
2023-05-27 17:23:21 +0700

    :recycle: Feat: refacto redux

--- src/slices/employees.slice.js
+++ src/slices/employees.slice.js
@@ -1,35 +1,16 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// export const userDatatSlice = createSlice({
-//   user: [],
-//   name: 'user',
-
-//   initialState: {
-//     user: [],
-//   },
-
-//   reducers: {
-//     setUsersData: (state, action) => {
-//       state.users = action.payload;
-//     },
-//   },
-// });
-
-// export const { setUsersData } = userDatatSlice.actions;
-// export default userDatatSlice.reducer;
-
 import { createSlice } from '@reduxjs/toolkit';
-
-const initialState = {
-  users: [],
-};
 
 const userSlice = createSlice({
   name: 'user',
-  initialState,
+  initialState: {
+    users: [],
+  },
   reducers: {
     setUsersData: (state, action) => {
-      state.users.push(action.payload);
+      return {
+        ...state,
+        users: [...state.users, action.payload],
+      };
     },
   },
 });
