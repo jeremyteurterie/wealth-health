fcfbc7036c79ed8a2c17e4de04da8b1f1c85b892
Jeremy <jeremy.teurterie@gmail.com>
2023-05-27 17:18:44 +0700

    :sparkles: Feat: refacto home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setUsersData } from '../slices/employees.slice';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
